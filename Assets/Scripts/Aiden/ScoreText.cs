ccdd12253b064ee6c33d741eafe73d1c0d17f270
Steppedcrown <92701553+Steppedcrown@users.noreply.github.com>
2026-08-03 10:58:29 -0700

    UI remove np and hide score (#6)
    * reduced unsolvable change to always be 0
    
    * removed np button
    
    * hid score text for all players

--- Assets/Scripts/Aiden/ScoreText.cs
+++ Assets/Scripts/Aiden/ScoreText.cs
@@ -5,12 +5,19 @@ public class ScoreText : MonoBehaviour
 {
     [SerializeField] private TMP_Text scoreText;
 
-    void Update()
+    /**void Update()
     {
+        if (PlayerRoleHolder.LocalRole != PlayerRole.RoleC)
+        {
+            scoreText.gameObject.SetActive(false);
+            return;
+        }
+        scoreText.gameObject.SetActive(true);
+
         if (ScaleManager.Instance == null || !TimerManager.Instance.IsSessionActive()) return;
 
         int score = Mathf.RoundToInt(ScaleManager.Instance.LiveScore);
         int bonusScore = Mathf.RoundToInt(ScaleManager.Instance.LiveBonusScore);
         scoreText.text = $"Score: {score}\nBonus Score: {bonusScore}";
-    }
+    }**/
 }
